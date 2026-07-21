e596e045f4e1432978a7e3496ee27b9773486471
Ylong4004 <3481980960@qq.com>
2026-07-21 16:12:42 +0800

    Cloudflare Workers Builds 配置已完成

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   // 如果你的 GitHub 仓库不是根目录，需要配置 base，例如仓库名叫 Fimel，就填 '/Fimel/'
   // 如果你有自定义域名或者是在 [你的名字].github.io 根目录，这里请保持为 '/'
-  base: '/Fimel/', 
+  base: '/',
   plugins: [react()],
 })
